0700a1e587235a3902dca771ed439ce2f81e6a70
Annie Truex <artruex@gmail.com>
2019-07-26 17:00:59 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(words)
+  counter = 0 
+  while counter < words.length 
+  yield(words[counter])
+  counter = counter +=1 
 end
+words
+end
+
