7492365fdf1bcf70c8b36ecafb7159ee76d24372
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-07-26 16:59:13 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,11 @@
 def my_each(array)
+  
   i = 0
  
   while i < array.length
-    yield array[i]
+   yield(array[i])
     i = i + 1
   end
   array
 end
-my_each(array) {|x| 
-  puts x}
-end
-end
+
