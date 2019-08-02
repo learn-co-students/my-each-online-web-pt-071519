27a7a0a8c50fd2443390ddd820d686e4d50dc504
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-08-02 02:56:33 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+array=["Caitlin","Taylor","Brooke"]
+def my_each (array) # put argument(s) here
+if block_given?
+i=0
+while i<array.length
+  yield(array[i])
+  i+=1
+end
+else
+  "There is no block given!"
+end
+array
 end
